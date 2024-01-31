c105d2e6d8988fa837f3a6f8576dfb5281a167d3
micheleguidaa <michele.2017guida@gmail.com>
2024-01-31 16:44:36 +0100

    Ci siamo quasi

---
+++ Assets/Scripts/GameSetup.cs
@@ -0,0 +1,102 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameSetup : MonoBehaviour
+{
+    int redBallsRemaining = 7;
+    int blueBallsRemaining = 7;
+    float ballRadius;
+    float ballDiameter;
+
+    [SerializeField] GameObject ballPrefab;
+    [SerializeField] Transform cueBallPosition;
+    [SerializeField] Transform headBallPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ballRadius = ballPrefab.GetComponent<SphereCollider>().radius * 100f;
+        ballDiameter = ballRadius * 2f;
+        PlaceAllBalls();
+    }
+
+    // Update is called once per frame
+    void PlaceAllBalls()
+    {
+        PlaceCueBall();
+        PlaceRandomBalls();
+    }
+
+    void PlaceCueBall()
+    {
+        GameObject ball = Instantiate(ballPrefab, cueBallPosition.position, Quaternion.identity);
+        ball.GetComponent<Ball>().MakeCueBall();
+    }
+
+    void PlaceEightBall(Vector3 position)
+    {
+        GameObject ball = Instantiate(ballPrefab, position, Quaternion.identity);
+        ball.GetComponent<Ball>().MakeEightBall();
+    }
+
+    void PlaceRandomBalls()
+    {
+        int NumInThisRow = 1;
+        int rand;
+        Vector3 firstInRowPosition = headBallPosition.position;
+        Vector3 currentPosition = firstInRowPosition;
+
+        void PlaceRedBall(Vector3 position)
+        {
+            GameObject ball = Instantiate(ballPrefab, position, Quaternion.identity);
+            ball.GetComponent<Ball>().BallSetup(true);
+            redBallsRemaining--;
+        }
+
+        void PlaceBlueBall(Vector3 position)
+        {
+            GameObject ball = Instantiate(ballPrefab, position, Quaternion.identity);
+            ball.GetComponent<Ball>().BallSetup(false);
+            blueBallsRemaining--;
+        }
+
+        
+        for(int i=0; i<5; i++)
+        {
+               for(int j=0; j < NumInThisRow; j++)
+            {
+                if(i==2 && j == 1)
+                {
+                    PlaceEightBall(currentPosition);
+                }
+                else if (redBallsRemaining > 0 && blueBallsRemaining > 0)
+                {
+                    rand = Random.Range(0, 2);
+                    if(rand == 0)
+                    {
+                        PlaceRedBall(currentPosition);
+                    }
+                    else
+                    {
+                        PlaceBlueBall(currentPosition);
+                    }
+                }
+                else if(redBallsRemaining > 0)
+                {
+                    PlaceRedBall(currentPosition);
+                }
+                else
+                {
+                    PlaceBlueBall(currentPosition);
+                }
+
+                currentPosition += new Vector3(1, 0, 0).normalized * ballDiameter;
+            }
+
+            firstInRowPosition += new Vector3(-1, 0, -1).normalized * ballDiameter;
+            currentPosition = firstInRowPosition;
+            NumInThisRow++;
+        }
+    }
+}
