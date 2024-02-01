b0443b392d9ea0e7cea3178548659b8358e0519e
micheleguidaa <michele.2017guida@gmail.com>
2024-02-01 19:00:48 +0100

    commit

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour
@@ -13,6 +14,7 @@ public class CameraController : MonoBehaviour
     private float horizontalInput;
     private bool isTakingShot = false;
     [SerializeField] float maxDrawDistance;
+    [SerializeField] TextMeshProUGUI powerText;
     private float savedMousePosition;
 
     Transform cueBall;
@@ -51,7 +53,7 @@ public class CameraController : MonoBehaviour
         }
         
         //End Temporary
-        */
+        
         if(Input.GetButtonDown("Fire1")&& gameObject.GetComponent<Camera>().enabled)
         {
             Vector3 hitDirection = transform.forward;
@@ -61,7 +63,8 @@ public class CameraController : MonoBehaviour
             cueStick.SetActive(false);
             gameManager.SwitchCameras();
         }
-
+        */
+        Shoot();
     }
 
     public void ResetCamera()
@@ -76,7 +79,36 @@ public class CameraController : MonoBehaviour
     {
         if(gameObject.GetComponent<Camera>().enabled)
         {
-            if(Input.GetButtonDown("Fire 1") && !isTakingShot)
+            if (Input.GetButtonDown("Fire1") && !isTakingShot)
+            {
+                isTakingShot = true;
+                savedMousePosition = 0f;
+            }
+            else if (isTakingShot)
+            {
+                if(savedMousePosition+Input.GetAxis("Mouse Y") <= 0)
+                {
+                    savedMousePosition += Input.GetAxis("Mouse Y");
+
+                    if(savedMousePosition<=maxDrawDistance)
+                    {
+                        savedMousePosition = maxDrawDistance;
+                    }
+                    float powerValueNumber = ((savedMousePosition - 0) / (maxDrawDistance - 0)) * (100 - 0) + 0;
+                    int powerValueInt = Mathf.RoundToInt(powerValueNumber);
+                    powerText.text = "Power: " + powerValueInt + "%";
+                }
+                if(Input.GetButtonDown("Fire1"))
+                {
+                    Vector3 hitDirection = transform.forward;
+                    hitDirection = new Vector3(hitDirection.x, 0, hitDirection.z).normalized;
+
+                    cueBall.gameObject.GetComponent<Rigidbody>().AddForce(hitDirection * power * Mathf.Abs(savedMousePosition), ForceMode.Impulse);
+                    cueStick.SetActive(false);
+                    gameManager.SwitchCameras();
+                    isTakingShot = false;
+                }
+            }
         }
     }
 }
