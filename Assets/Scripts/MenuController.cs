41a6bbf3fdf486a10bee41114ea7df14c6e165f2
micheleguidaa <michele.2017guida@gmail.com>
2024-02-05 15:21:38 +0100

    dai pippo

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -3,14 +3,33 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class MenuController : MonoBehaviour
 {
+    [Header("Volume Settings")]
+    [SerializeField] private TMP_Text volumeTextValue = null;
+    [SerializeField] private Slider volumeSlider = null;
+    [SerializeField] private float defaultVolume = 1.0f;
+
+    [Header("Gameplay Settings")]
+    [SerializeField] private TMP_Text controllerCamSensTextValue = null;
+    [SerializeField] private TMP_Text controllerShotSensTextValue = null;
+    [SerializeField] private Slider controllerCamSensSlider = null;
+    [SerializeField] private Slider controllerShotSensSlider = null;
+    [SerializeField] private int defaultCamSens = 4;
+    [SerializeField] private int defaultShotSens = 4;
+    public int mainControllerSens1 = 4;
+    public int mainControllerSens2 = 4;
+
     [Header("Levels To Load")]
     public string newGameLevel;
     private string levelToLoad;
     [SerializeField] private GameObject noSavedGameDialog = null;
-    
+
+    [Header("Confirmation")]
+    [SerializeField] private GameObject confirmationPrompt = null;
+
     public void NewGameDialogYes()
     {
         SceneManager.LoadScene(newGameLevel);
@@ -32,6 +51,63 @@ public class MenuController : MonoBehaviour
     public void ExitButton()
     {
         Application.Quit();
+    }
+
+    public void SetVolume(float volume)
+    {
+        AudioListener.volume = volume;
+        volumeTextValue.text = volume.ToString("0.0");
+    }
+
+    public void VolumeApply()
+    {
+        PlayerPrefs.SetFloat("masterVolume",AudioListener.volume);
+        StartCoroutine(ConfirmationBox());
+    }
+
+    public void SetCameraSens(float value)
+    {
+        mainControllerSens1 = Mathf.RoundToInt(value);
+        controllerCamSensTextValue.text = value.ToString("0");
+    }
+
+    public void SetShotaSens(float value)
+    {
+        mainControllerSens2 = Mathf.RoundToInt(value);
+        controllerShotSensTextValue.text = value.ToString("0");
+    }
+
+    public void GameplayApply()
+    {
+        PlayerPrefs.SetFloat("masterCamSens", mainControllerSens1);
+        PlayerPrefs.SetFloat("masterShotSens", mainControllerSens2);
+        StartCoroutine(ConfirmationBox());
+    }
+
+    public void ResetButton(string menuType)
+    {
+        if(menuType == "Audio")
+        {
+            AudioListener.volume = defaultVolume;
+            volumeSlider.value = defaultVolume;
+            volumeTextValue.text = defaultVolume.ToString("0.0");
+            VolumeApply();
+        }
+        if(menuType == "GamePlay")
+        {
+            controllerShotSensTextValue.text = defaultShotSens.ToString("0");
+            controllerShotSensSlider.value = defaultShotSens;
+            controllerCamSensSlider.value = defaultCamSens;
+            controllerCamSensTextValue.text = defaultCamSens.ToString("0");
+            GameplayApply();
+        }
+    }
+
+    public IEnumerator ConfirmationBox()
+    {
+        confirmationPrompt.SetActive(true);
+        yield return new WaitForSeconds(2);
+        confirmationPrompt.SetActive(false);
     }
 
 }
